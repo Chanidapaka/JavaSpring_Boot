82ad3f2f9fd5f8ba91bf671b28ac76c2f2b6dee0
INT202 <INT202@Train10-3-PC243.control.ad.sit.kmutt.ac.th>
2024-10-08 15:52:16 +0700

    Update office_list.HTML Make a link 2

--- demo2/src/main/java/sit/int202/demo/controllers/OfficeController.java
+++ demo2/src/main/java/sit/int202/demo/controllers/OfficeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import sit.int202.demo.entities.Office;
 import sit.int202.demo.repositories.OfficeRepository;
 import sit.int202.demo.services.OfficeService;
@@ -12,13 +13,22 @@ import sit.int202.demo.services.OfficeService;
 import java.util.List;
 
 @Controller
-@RequestMapping("/offices") //รับrequest ทุกชนิด --เราจะ/ไปทางนี้ก่อน1
+@RequestMapping("/offices") // กำหนด URL ที่จะใช้สำหรับ Controller นี้
 public class OfficeController {
     private final OfficeService service;
 
+    // Constructor Injection เพื่อฉีด OfficeService
     public OfficeController(OfficeService service) {
         this.service = service;
     }
+    @GetMapping("") // เมธอดที่จะรับคำขอ GET ที่ /offices
+        public String getOfficeById(@RequestParam String officeCode, Model model) {
+        // ดึงข้อมูลออฟฟิศตาม officeCode
+            Office office = service.getOffice(officeCode);
+            model.addAttribute("office", office);
+            return "office_details";
+        }
+
 
     @GetMapping("/all") //เอาแค่get   --เราจะ/ไปทางนี้ทีหลัง2
     public String allOffices(Model model) {
